64fa3afda20379f7beefb6c824a6a634fc7bba93
dave-cloud <ooluwaseunfilani@gmail.com>
2026-05-17 13:44:10 +0100

    Fix main terrafile

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -5,25 +5,18 @@
 terraform {
   required_version = ">= 1.5.0"
 
-
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.0"
-
     }
-
   }
 
+  backend "s3" {
+    bucket       = "dave-cloud-resume-tfstate-178845344404"
+    key          = "cloud-resume/terraform.tfstate"
+    region       = "us-east-1"
+    use_lockfile = true
+    encrypt      = true
+  }
 }
-
- terraform {
-   backend "s3" {
-    bucket         = "dave-terraform-state-bucket"
-    key            = "cloud-resume/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "terraform-locks"
-    encrypt        = true
-
-     }
-   }
